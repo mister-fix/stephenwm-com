adf2782ae670a07e3245f8def351b63a81ab84fb
Stephen <hellostephenwm@gmail.com>
2025-07-09 23:12:00 -0400

    chore: format file with updated prettier settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,35 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+	variable: '--font-geist-sans',
+	subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+	variable: '--font-geist-mono',
+	subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Stephen M. – Software Engineer ",
-  description: "Portfolio of Stephen Mwingira, a Software Engineer based out of New Jersey, USA.",
+	title: 'Stephen M. – Software Engineer ',
+	description:
+		'Portfolio of Stephen Mwingira, a Software Engineer based out of New Jersey, USA.',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+			>
+				{children}
+			</body>
+		</html>
+	);
 }

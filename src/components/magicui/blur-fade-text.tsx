17683531beaac54edfdb35f0f95bf59300e964ae
Stephen <hellostephenwm@gmail.com>
2025-08-21 01:06:46 -0400

    chore: fix text blurring loop error

--- src/components/magicui/blur-fade-text.tsx
+++ src/components/magicui/blur-fade-text.tsx
@@ -45,7 +45,6 @@ export const BlurFadeText = ({
 							exit="hidden"
 							variants={combinedVariants}
 							transition={{
-								repeat: Infinity,
 								delay: delay + i * characterDelay,
 								ease: 'easeOut',
 							}}
@@ -69,7 +68,6 @@ export const BlurFadeText = ({
 					exit="hidden"
 					variants={combinedVariants}
 					transition={{
-						repeat: Infinity,
 						delay,
 						ease: 'easeOut',
 					}}
